45befa17d7098f1ec8caa7d50d79b0e0112749f9
Timboy67678 <me@timboy67678.com>
2016-09-12 22:56:14 +1000

    more updates

--- DeployCommand.cs
+++ DeployCommand.cs
@@ -1,22 +1,51 @@
 ﻿using ManyConsole;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace BuildDeploy
 {
     public class DeployCommand : ConsoleCommand
     {
-        private string WorkingDir, DeployAddress, FilesExtension;
+        private string FileOrWorkingDirectory, DeployHost, DeployPort, FilesExtension;
 
         public DeployCommand()
         {
             IsCommand("deploy", "Deploy applications from directory.");
 
-            HasRequiredOption("dir|directory=", "Application deploy directory", dir => WorkingDir = dir);
-            HasRequiredOption("ext|extension=", "Target files extension", ext => FilesExtension = ext);
-            HasRequiredOption("addr|address=", "Target deploying address", addr => DeployAddress = addr);
+            HasRequiredOption("dir|directory=", "Deploy from directory", dir => FileOrWorkingDirectory = dir);
+            HasRequiredOption("host=", "Target deploying host", host => DeployHost = host);
+            HasOption("ext|extension=", "Targeted files extension", ext => FilesExtension = ext);
+            HasOption("port=", "Target deploying port number", port => DeployPort = port);
         }
 
         public override int Run(string[] remainingArguments)
         {
+            if (string.IsNullOrEmpty(FilesExtension))
+                FilesExtension = Program.DefaultExtensionSearch;
+
+            if (string.IsNullOrEmpty(DeployPort))
+                DeployPort = Program.DefaultDeployPort;
+
+            FileAttributes attr;
+            List<string> file_names = new List<string>();
+
+            try {
+                attr = File.GetAttributes(FileOrWorkingDirectory);
+            } catch(IOException e)  {
+                Console.WriteLine(string.Format("Error: {0}", e.Message));
+                return 1;
+            }
+
+            if (attr.HasFlag(FileAttributes.Directory))
+                file_names.AddRange(Directory.GetFiles(FileOrWorkingDirectory, string.Format("*.{0}", FilesExtension)));
+            else
+                file_names.Add(FileOrWorkingDirectory);
+            
+            foreach(var file in file_names)
+            {
+                Console.WriteLine(file);
+            }
             
             return 0;
         }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ namespace BuildDeploy
 {
     class Program
     {
+        public const string DefaultExtensionSearch = "exe";
+        public const string DefaultDeployPort = "10102";
+
         private static int Main(string[] args)
         {
             var items = ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(Program));
